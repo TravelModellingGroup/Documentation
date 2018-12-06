e36fc6ca48d314dbf47523ac0ae05025a49503dd
Brendan Reilly <brendan.benting@gmail.com>
2018-12-06 13:23:28 -0500

    Updated ModuleDoc src and packages Updated gitignore

--- tmg-documentation/ModuleDocProcessor/ModuleDocProcessor/ModueDocProcessor.cs
+++ tmg-documentation/ModuleDocProcessor/ModuleDocProcessor/ModueDocProcessor.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.DocAsCode.Plugins;
@@ -12,28 +14,61 @@ namespace ModuleDocProcessor
     [Export(typeof(IDocumentProcessor))]
     public class ModuleDocProcessor : IDocumentProcessor
     {
+        /// <summary>
+        /// Notify that that this document processor will process .NET assemblies (.dll)
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         public ProcessingPriority GetProcessingPriority(FileAndType file)
         {
-            throw new NotImplementedException();
+            if (file.Type == DocumentType.Article &&
+                ".dll".Equals(Path.GetExtension(file.File), StringComparison.OrdinalIgnoreCase))
+            {
+                return ProcessingPriority.Normal;
+            }
+            return ProcessingPriority.NotSupported;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="metadata"></param>
+        /// <returns></returns>
         public FileModel Load(FileAndType file, ImmutableDictionary<string, object> metadata)
         {
-            throw new NotImplementedException();
+            Assembly assembly = Assembly.LoadFrom(Path.Combine(file.BaseDir, file.File));
+            
+            return new FileModel(file,assembly);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public SaveResult Save(FileModel model)
         {
-            throw new NotImplementedException();
+            return new SaveResult
+            {
+                DocumentType = "Conceptual",
+                FileWithoutExtension = Path.ChangeExtension(model.File, null),
+            };
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="context"></param>
         public void UpdateHref(FileModel model, IDocumentBuildContext context)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
-        public string Name { get; }
+        public string Name  => nameof(ModuleDocProcessor);
 
-        public IEnumerable<IDocumentBuildStep> BuildSteps { get; }
+        [ImportMany(nameof(ModuleDocProcessor))]
+        public IEnumerable<IDocumentBuildStep> BuildSteps { get; set; }
     }
 }
---
+++ tmg-documentation/ModuleDocProcessor/ModuleDocProcessor/ModuleDocBuildStep.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
+using Microsoft.DocAsCode.Plugins;
+
+namespace ModuleDocProcessor
+{
+    [Export(nameof(ModuleDocProcessor), typeof(IDocumentBuildStep))]
+    public class ModuleDocBuildStep : IDocumentBuildStep
+    {
+        private readonly TaskFactory _taskFactory = new TaskFactory(new StaTaskScheduler(1));
+
+        public IEnumerable<FileModel> Prebuild(ImmutableList<FileModel> models, IHostService host)
+        {
+            return models;
+        }
+
+        public void Build(FileModel model, IHostService host)
+        {
+            model.Content = "transform test";
+        }
+
+        public void Postbuild(ImmutableList<FileModel> models, IHostService host)
+        {
+            return;
+        }
+
+        public string Name => nameof(ModuleDocBuildStep);
+
+
+        public int BuildOrder => 0;
+    }
+}
