0bb6514753df94349cb906ab30c2ff9fe68e0b7c
Brendan Reilly <brendan.benting@gmail.com>
2018-12-06 12:44:20 -0500

    Added ModuleDocProcessor project / template

---
+++ tmg-documentation/ModuleDocProcessor/ModuleDocProcessor/ModueDocProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DocAsCode.Plugins;
+
+namespace ModuleDocProcessor
+{
+    [Export(typeof(IDocumentProcessor))]
+    public class ModuleDocProcessor : IDocumentProcessor
+    {
+        public ProcessingPriority GetProcessingPriority(FileAndType file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileModel Load(FileAndType file, ImmutableDictionary<string, object> metadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SaveResult Save(FileModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHref(FileModel model, IDocumentBuildContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name { get; }
+
+        public IEnumerable<IDocumentBuildStep> BuildSteps { get; }
+    }
+}
