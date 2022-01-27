d4933334f8322ea2550d3a6d788eebf3342d16b8
James Vaughan <James.Vaughan@utoronto.ca>
2022-01-27 11:02:08 -0500

    Added Version 2.0 and stubs for XTMF2 and TMGToolbox2. (#24)
    Removed old files.

--- 2.0/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Travel Modelling Group Documentation"],titleterms:{document:0,group:0,indic:0,model:0,tabl:0,travel:0,welcom:[]}})
---
+++ tmg-documentation/2.0/XTMF2/index.md
@@ -0,0 +1,3 @@
+# Welcome to XTMF2
+
+TODO: Under construction
